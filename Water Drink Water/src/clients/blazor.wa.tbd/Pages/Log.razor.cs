709cb91d2175ceedf570012b0e79a375eb9c4d06
CodeItQuick <evanontario@hotmail.com>
2024-05-20 10:33:02 -0300

    add Log Water Consumption Per Day onto Log Water Consumption Widget

--- Water Drink Water/src/clients/blazor.wa.tbd/Pages/Log.razor.cs
+++ Water Drink Water/src/clients/blazor.wa.tbd/Pages/Log.razor.cs
@@ -9,6 +9,20 @@ public partial class Log
     [CascadingParameter] public ConsumptionStateProvider ConsumptionState { get; set; } = null!;
     
     private int FluidOuncesConsumed { get; set; } = 0;
+    private DateTimeOffset LogDatePicker { get; set; } = DateTimeOffset.Now;
+    private Dictionary<(string, string, string), List<(string, string)>> Logs { get; set; } = new()
+    {
+        [("2024", "5", "20")] = new ()
+        {
+            ("8:30 am", "1 L"),
+            ("9:30 am", "0.5 L"),
+            ("11:30 am", "2 L")
+        },
+        [("2024", "5", "19")] = new ()
+        {
+            ("6:30 am", "1 Glass")
+        },
+    };
 
     private async Task LogConsumption()
     {
